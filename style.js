0033ab80564ddd8ad7a5f6dc1669e8f40301b3b3
portifolio <carvalho.shotokan21@gmail.com>
2023-09-28 00:18:59 -0300

    nova mod

---
+++ style.js
@@ -0,0 +1,83 @@
+//Imagens do projeto em breve
+//embreve1
+var project=document.getElementById("embreve1");
+var caminhoimg1="img/embreve1.png";
+var caminhoimg2="img/embreve2.png";
+
+project.addEventListener("mouseover", function() {
+    project.src =caminhoimg2;});
+project.addEventListener("mouseout", function(){
+    project.src=caminhoimg1;
+ //embreve2   
+});
+var project2=document.getElementById("embreve2");
+var caminhoimg10="img/embreve1.png";
+var caminhoimg20="img/embreve2.png";
+
+project2.addEventListener("mouseover", function() {
+    project2.src =caminhoimg20;});
+project2.addEventListener("mouseout", function(){
+    project2.src=caminhoimg10;
+});
+//embreve3
+var project3=document.getElementById("embreve3");
+var caminhoimg100="img/embreve1.png";
+var caminhoimg200="img/embreve2.png";
+
+project3.addEventListener("mouseover", function() {
+    project3.src =caminhoimg200;});
+project3.addEventListener("mouseout", function(){
+    project3.src=caminhoimg100;
+});
+//embreve4
+var project4=document.getElementById("embreve4");
+var caminhoimg1000="img/embreve1.png";
+var caminhoimg2000="img/embreve2.png";
+
+project4.addEventListener("mouseover", function() {
+    project4.src =caminhoimg2000;});
+project4.addEventListener("mouseout", function(){
+    project4.src=caminhoimg1000;
+});
+//embreve 5
+var project5=document.getElementById("embreve5");
+var caminhoimg10000="img/embreve1.png";
+var caminhoimg20000="img/embreve2.png";
+
+project5.addEventListener("mouseover", function() {
+    project5.src =caminhoimg20000;});
+project5.addEventListener("mouseout", function(){
+    project5.src=caminhoimg10000;
+});
+//embreve6
+var project6=document.getElementById("embreve6");
+var caminhoimg100000="img/embreve1.png";
+var caminhoimg200000="img/embreve2.png";
+
+project6.addEventListener("mouseover", function() {
+    project6.src =caminhoimg200000;});
+project6.addEventListener("mouseout", function(){
+    project6.src=caminhoimg100000;
+});
+//embreve7
+var project7=document.getElementById("embreve7");
+var caminhoimgum="img/embreve1.png";
+var caminhoimgdois="img/embreve2.png";
+
+project7.addEventListener("mouseover", function() {
+    project7.src =caminhoimgdois;});
+project7.addEventListener("mouseout", function(){
+    project7.src=caminhoimgum;
+});
+//embreve8
+var project8=document.getElementById("embreve8");
+var caminhoimgum1="img/embreve1.png";
+var caminhoimgdois1="img/embreve2.png";
+
+project8.addEventListener("mouseover", function() {
+    project8.src =caminhoimgdois1;});
+project8.addEventListener("mouseout", function(){
+    project8.src=caminhoimgum1;
+});
+
+
